bd07154a19936edb843db3c8aca2e2f48577528c
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-01 23:06:51 +0300

    Created InformationDialog from Dialog (at common part)

---
+++ composeApp/src/androidMain/kotlin/com/jetbrains/kmpapp/components/InformationDialog.kt
@@ -0,0 +1,20 @@
+package com.jetbrains.kmpapp.components
+
+import androidx.compose.runtime.Composable
+import com.jetbrains.kmpapp.ui.components.Button
+import com.jetbrains.kmpapp.ui.components.Dialog
+
+class InformationDialog : Dialog {
+//    TODO: need to understand how to use here @Composable for android part
+    override fun render() {
+        // Text
+        // Button
+        // Or two buttons
+        TODO("Not yet implemented")
+    }
+
+    override fun createButton(text: String): Button {
+        TODO("Not yet implemented")
+    }
+
+}
